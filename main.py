241b1f27137e6d8b056c52db0bf54f5074f9d776
kvmoraes <karinevitoria.marinho@gmail.com>
2023-03-29 00:55:59 -0300

    refactor: change video exibition by using VLC Media Player

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-from playwright.sync_api import sync_playwright
+import vlc
+import pafy
 import time
 
 now = str
@@ -14,18 +15,15 @@ def adjust_volume():
     volume = cast(interface, POINTER(IAudioEndpointVolume))
     volume.SetMasterVolumeLevel(1, None)
 
-def start_alarm(playwright):
+def start_alarm():
     adjust_volume()
 
-    chromium = playwright.chromium
-    browser = chromium.launch(headless= False)
-    page = browser.new_page()
-    page.goto(url= 'https://www.youtube.com/watch?v=uWBo10vVxwo/',
-        wait_until= 'domcontentloaded')
-    time.sleep(1)
-    page.locator("video").click(click_count= 2, delay= 0.7)
+    url = 'https://www.youtube.com/watch?v=uWBo10vVxwo'
+    video = pafy.new(url)
+    best = video.getbest()
+    media = vlc.MediaPlayer(best.url)
+    media.play()
     time.sleep(3*60)
-    browser.close()
 
 print("Selecione a hora em que deseja que o alarme toque (Ex. 06:30)")
 userInput = input(">> ")
@@ -33,6 +31,5 @@ while True:
     now = datetime.now()
 
     if now.strftime("%H:%M") == userInput:
-        with sync_playwright() as playwright:
-            start_alarm(playwright)
+        start_alarm()
         break
